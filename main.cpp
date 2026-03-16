54b6f0a3e45eef9645f382d81ef45b6b87bc8de8
Austin <austintemby007@gmail.com>
2026-03-16 18:15:15 +0000

    hi

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "C++ environment is working!" << endl;
+
+    int a = 5;
+    int b = 7;
+
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+    cout << "a + b = " << a + b << endl;
+
+    return 0;
+}
