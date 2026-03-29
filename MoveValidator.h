ce92060cc9ba20e3b9b8e8e56879d49320d8ea44
HimeshOH <himeshshipair@gmail.com>
2026-03-29 18:59:04 -0400

    MoveValidator.h

--- MoveValidator.h
+++ MoveValidator.h
@@ -1,14 +1,17 @@
 #ifndef MOVEVALIDATOR_H
 #define MOVEVALIDATOR_H
 
-#include "GameBoard.h"
+#include "GameBoard.h" // needed because this class checks the board
 
-// This class checks game rules (win or draw)
+// This class checks the rules of the game
 class MoveValidator
 {
 public:
     bool hasWinner(const GameBoard& board, char mark) const;
+    // checks if a player with mark X or O has won
+
     bool isDrawGame(const GameBoard& board) const;
+    // checks if the board is full and game is a draw
 };
 
 #endif
