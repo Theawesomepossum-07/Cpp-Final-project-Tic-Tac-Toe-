058a15fd6e9b64a3dcc76f9fff506385ec046ed7
Lux3007 <lakshithashree7@gmail.com>
2026-03-29 18:59:06 -0400

    Update playerinfo.cpp

--- playerinfo.cpp
+++ playerinfo.cpp
@@ -1,15 +1,16 @@
 #include "playerinfo.h"
-
+// Includes the heard file
 playerinfo::playerinfo(std::string n, char m, int id) {
     Name = n;
     xoro = m;
     ID = id;
-}
+} // Constructor
 
 void playerinfo::setName(std::string n) { Name = n; }
 void playerinfo::setxoro(char m) { xoro = m; }
 void playerinfo::setID(int i) { ID = i; }
-
+// Assign n,m,i to name, xoro and ID
 std::string playerinfo::getName() const { return Name; }
 char playerinfo::getxoro() const { return xoro; }
 int playerinfo::getID() const { return ID; }
+// The getters returns all the values}
