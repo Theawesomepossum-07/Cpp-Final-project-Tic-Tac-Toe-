ea3a643341a7df9e22ee3725ad905d0b8ed0a6b0
Lux3007 <lakshithashree7@gmail.com>
2026-03-29 19:01:36 -0400

    Update scores.h

--- scores.h
+++ scores.h
@@ -1,22 +1,22 @@
 #pragma once
-
+// To prevent multiple inclusion
 class scores
 {
 private:
     int p1win;
     int p2win;
     int tie;
-
+// The type of wins are defined
 public:
-scores(int p1, int p2, int t);
+scores(int p1, int p2, int t); //Constructor
 
-void addwin(int);
-void addDraw();
+void addwin(int); // Counts the wins
+void addDraw(); // Counts the draws
 
 
     int getp1win() const;
     int getp2win() const;
     int gettie() const;
-
-void printscore() const;
+// Getters
+void printscore() const; // Prints the scores
 };
