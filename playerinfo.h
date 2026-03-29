0ac4a58ced27970fdba5ec62874b37d97e5f83e7
Lux3007 <lakshithashree7@gmail.com>
2026-03-29 18:58:26 -0400

    Update playerinfo.h

--- playerinfo.h
+++ playerinfo.h
@@ -1,20 +1,21 @@
 #pragma once
-#include <string>
-
+// To prevent multiple inclusion
+#include <string> 
+// To use string
 class playerinfo {
 private:
     std::string Name;
     char xoro;
     int ID;
-
+// Defined name, piece and id
 public:
-    playerinfo() { Name = " "; xoro = ' '; ID = 0; }
+    playerinfo() { Name = " "; xoro = ' '; ID = 0; } // Default constructor
     playerinfo(std::string n, char m, int id); // Added std:: here
-
+// Getters
     std::string getName() const;
     char getxoro() const;
     int getID() const;
-
+// Setters
     void setName(std::string n);
     void setxoro(char m);
     void setID(int i);
