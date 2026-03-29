c1595cfdbc9238222e6654a4f110896d4a43ba39
Lux3007 <lakshithashree7@gmail.com>
2026-03-29 19:00:39 -0400

    Update input.cpp

--- input.cpp
+++ input.cpp
@@ -1,16 +1,16 @@
 #include "input.h"
-
-input::input(int s) : size(s) {}
+// Includes the header file 
+input::input(int s) : size(s) {} // Constructor
 
 int input::grid(int mx, int my) 
 {
     int column = mx / size;
 int row = my / size;
-
+// Determine the row and column of the click
     if (column < 0 || column > 2 || row < 0 || row > 2)
     {
         return -1;
-    }
+    } // Incase, the click is outside 
 
-    return row * 3 + column;
+    return row * 3 + column; // Converts the row and coloumnn into a single index
 }
