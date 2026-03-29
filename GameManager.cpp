d054ac939df0f799ed0b5e9d8d0513d33f46ddb6
HimeshOH <himeshshipair@gmail.com>
2026-03-29 18:57:58 -0400

    GameManager.cpp

--- GameManager.cpp
+++ GameManager.cpp
@@ -1,80 +1,92 @@
-#include "GameManager.h"
+#include "GameManager.h" // connects this file to GameManager class
 
 // constructor
+// this runs when GameManager object is created
 GameManager::GameManager()
 {
-    newGame();
+    newGame(); // start with fresh game
 }
 
-// start or reset game
+// this starts or resets the game
 void GameManager::newGame()
 {
-    board.clearBoard();
-    activePlayer = 'X';
-    finished = false;
-    result = ' ';
+    board.clearBoard(); // make board empty
+    activePlayer = 'X'; // X starts first
+    finished = false; // game is not over
+    result = ' '; // no result yet
 }
 
-// handle a player's move
+// this handles one player's move
 bool GameManager::playTurn(int row, int col)
 {
+    // if game already ended, do not allow more moves
     if (finished)
         return false;
 
-    // try placing move
+    // try to place the move on the board
+    // if move fails, return false
     if (!board.addMove(row, col, activePlayer))
         return false;
 
-    // check winner
+    // after placing move, check if current player won
     if (checker.hasWinner(board, activePlayer))
     {
-        finished = true;
-        result = activePlayer;
+        finished = true; // game ends
+        result = activePlayer; // winner is current player
         return true;
     }
 
-    // check draw
+    // if no winner, check if game is a draw
     if (checker.isDrawGame(board))
     {
-        finished = true;
-        result = 'D';
+        finished = true; // game ends
+        result = 'D'; // D means draw
         return true;
     }
 
-    // switch turn if game continues
+    // if game is still going, switch to next player
     changePlayer();
+
     return true;
 }
 
-// switch between X and O
+// this changes turn between X and O
 void GameManager::changePlayer()
 {
+    // if current player is X, next is O
+    // otherwise next is X
     activePlayer = (activePlayer == 'X') ? 'O' : 'X';
 }
 
-// getters
+// getter for current player
 char GameManager::getCurrentPlayer() const
 {
     return activePlayer;
 }
 
+// getter to check if game is done
 bool GameManager::isFinished() const
 {
     return finished;
 }
 
+// getter to return winner or draw
 char GameManager::getResult() const
 {
     return result;
 }
 
+// getter to return board object
 const GameBoard& GameManager::getBoard() const
 {
     return board;
 }
-void GameManager::reset() {
-    board.clearBoard(); // This calls your GameBoard's clear function
-    finished = false;   // Reset your win/loss flag
-    result = ' ';       // Reset the result character
-    // If you track whose turn it is here, reset that to Player 1 too!
+
+// this resets some game values
+void GameManager::reset()
+{
+    board.clearBoard(); // clear board
+    finished = false;   // game not over anymore
+    result = ' ';       // clear old result
+    // if needed, you can also put activePlayer = 'X'; here
 }
