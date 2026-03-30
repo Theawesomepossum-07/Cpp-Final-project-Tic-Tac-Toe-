2d7988aacd959cd81d92246bc3a595058c8dba51
Lux3007 <lakshithashree7@gmail.com>
2026-03-29 21:15:09 -0400

    Update MenuScreen.cpp

--- MenuScreen.cpp
+++ MenuScreen.cpp
@@ -1,6 +1,6 @@
 #include "MenuScreen.h"
-
-using namespace std;
+// Includes the header file
+using namespace std; // To not use std multiple times
 
 // Constructor: Initializes the menu screen with the given window dimensions
 MenuScreen::MenuScreen(int width, int height) {
@@ -13,13 +13,13 @@ void MenuScreen::drawTitle() {
     cout << "==============================\n";
     cout << "         Tic-Tac-Toe!   \n";
     cout << "==============================\n";
-}
+} // Displays the title
 
 void MenuScreen::drawStartButton() {
     
     cout << "          [Start Game]          \n";
     cout << "================================\n";
-}
+} // Displays the start of the game
 
 string MenuScreen::promptPlayerName(int playerNumber) {
     string playerName;
@@ -30,9 +30,9 @@ cout << "Player " << playerNumber << ", please enter your name: ";
 cin >> playerName;
 
 return playerName;
-}
+} // For the playername to be inputted 
 
 bool MenuScreen::isStartButtonClicked(int mouseX, int mouseY) {
 
 return true;
-}
+} // To start the game
