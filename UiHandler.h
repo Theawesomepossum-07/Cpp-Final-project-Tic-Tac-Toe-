bfb8e4a7ab15ebc6f9f5c2e58c0818e9d32e355e
Lux3007 <lakshithashree7@gmail.com>
2026-03-29 21:22:57 -0400

    Update UiHandler.h

--- UiHandler.h
+++ UiHandler.h
@@ -1,29 +1,28 @@
 #pragma once
+// To prevent multiple inclusion
+#include <SFML/Graphics.hpp> // To use the SFML graphics
+#include <iostream> // To follow the commands
+#include <string> // To use string
 
-#include <SFML/Graphics.hpp>
-#include <iostream>
-#include <string>
-
-struct Point {
-    int x;
+struct Point 
+{
+    int x; 
     int y;
-};
+}; // The x and y coordinate for the mouse click position
 
 class UIHandler {
 private:
-    int windowWidth;
+    int windowWidth; 
     int windowHeight;
-    
+    // Defined for the width and height of the game in pixels
     // NEW: SFML objects to handle the font and text
     sf::Font font;
     sf::Text resultText;
-
 public:
-    UIHandler(int width, int height);
+    UIHandler(int width, int height); // Constructor
+    void drawGrid(sf::RenderWindow& window); // To draw the tic-tac-toe
+    void drawMarker(sf::RenderWindow& window, int gridIndex, char marker); // To draw an X or O
+    void displayResult(sf::RenderWindow& window, std::string resultMessage); // To display the result
 
-    void drawGrid(sf::RenderWindow& window);
-    void drawMarker(sf::RenderWindow& window, int gridIndex, char marker);
-    void displayResult(sf::RenderWindow& window, std::string resultMessage);
-
-    int getSquareFromClick(Point clickPos);
+    int getSquareFromClick(Point clickPos); // For the mouse click position
 };
